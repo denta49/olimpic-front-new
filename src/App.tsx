c6592b2f7bec1b40e61777161c0fe3d6dce5d547
Przemysław Wojenka <p.wojenka@361.sh>
2025-02-19 20:49:23 +0100

    basic routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import StudentPage from "./pages/StudentPage";
+import CoordinatorPage from "./pages/CoordinatorPage";
+import AdminPage from "./pages/AdminPage";
+import RequireAuth from "./components/RequireAuth";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import NewPasswordPage from "./pages/NewPasswordPage";
+
 function App() {
   return (
-    <>
-      <div>
-        <Button>Testtttt</Button>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="reset-password" element={<ResetPasswordPage />} />
+        <Route path="new-password" element={<NewPasswordPage />} />
+
+        <Route element={<RequireAuth allowedRole="student" />}>
+          <Route path="student" element={<StudentPage />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRole="coordinator" />}>
+          <Route path="coordinator" element={<CoordinatorPage />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRole="admin" />}>
+          <Route path="admin" element={<AdminPage />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
